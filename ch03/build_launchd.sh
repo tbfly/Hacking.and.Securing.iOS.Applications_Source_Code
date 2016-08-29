bd9ee54a75d2a2609c011577ba8cbb58536e4632
Lu ZhiHe <luzhihe@gmail.com>
2016-08-29 15:29:18 +0800

    *): Make launchd link work.

--- ch03/build_launchd.sh
+++ ch03/build_launchd.sh
@@ -5,12 +5,12 @@ export PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/
    -arch armv7 -c syscalls.S -o syscalls.o
 
 /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang \
-   -arch armv7 -c launchd.c -o launchd.o \
+   -arch armv7 -fno-stack-protector -c launchd.c -o launchd.o \
    -isysroot $PLATFORM/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
    -I$PLATFORM/Developer/SDKs/iPhoneOS.sdk/usr/include \
    -I.
 
 /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang \
     -arch armv7 -o launchd launchd.o syscalls.o \
-    -static -nostartfiles -nodefaultlibs -nostdlib -Wl,-e,_main
+    -static -nostdlib -Wl,-e,_main
 
